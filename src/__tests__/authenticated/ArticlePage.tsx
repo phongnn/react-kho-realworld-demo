4bf12ce629aaf7dd51505e1004d868d7a80d4f7e
phongnn <phongnn@gmail.com>
2020-10-02 10:32:29 +0800

    NewArticlePage test cases

--- src/__tests__/authenticated/ArticlePage.tsx
+++ src/__tests__/authenticated/ArticlePage.tsx
@@ -1,3 +1,4 @@
+import { act } from "react-dom/test-utils"
 import {
   screen,
   waitFor,
@@ -5,11 +6,9 @@ import {
 } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 
-import { saveAccessToken, removeAccessToken } from "../../accessToken"
-import { renderRoute } from "../../../testUtils/render"
+import { renderProtectedRoute } from "../../../testUtils/render"
 import {
   allArticles,
-  accessToken,
   aliceArticles,
   bobArticles,
   alice,
@@ -20,17 +19,13 @@ import {
   rest as mswRest,
 } from "../../../testUtils/mocks/server"
 import config from "../../common/config"
-import { act } from "react-dom/test-utils"
 
 const { baseUrl } = config.api
-
-beforeAll(() => saveAccessToken(accessToken))
-afterAll(removeAccessToken)
 
 it("toggles article's Favorite status", async () => {
   const { slug, favoriteCount } = allArticles[0]
 
-  renderRoute(`/articles/${slug}`)
+  renderProtectedRoute(`/articles/${slug}`)
   await waitForElementToBeRemoved(screen.getByText(/loading/i))
 
   // prettier-ignore
@@ -42,7 +37,7 @@ it("toggles article's Favorite status", async () => {
 })
 
 it("toggles state of following author", async () => {
-  renderRoute(`/articles/${bobArticles[0].slug}`)
+  renderProtectedRoute(`/articles/${bobArticles[0].slug}`)
   await waitForElementToBeRemoved(screen.getByText(/loading/i))
 
   // prettier-ignore
@@ -56,7 +51,7 @@ it("toggles state of following author", async () => {
 it("goes to article edit screen when click on edit button", async () => {
   const { slug, title } = aliceArticles[0]
 
-  renderRoute(`/articles/${slug}`)
+  renderProtectedRoute(`/articles/${slug}`)
   await waitForElementToBeRemoved(screen.getByText(/loading/i))
   userEvent.click(
     (await screen.findAllByRole("link", { name: /Edit Article/ }))[0]
@@ -70,7 +65,7 @@ it("goes to article edit screen when click on edit button", async () => {
 it("deletes article and goes to user's page when click on delete button", async () => {
   const { slug, title } = aliceArticles[0]
 
-  renderRoute(`/articles/${slug}`)
+  renderProtectedRoute(`/articles/${slug}`)
   userEvent.click(
     (await screen.findAllByRole("button", { name: /Delete Article/ }))[0]
   )
@@ -84,7 +79,7 @@ describe("creating comment", () => {
   const { slug } = bobArticles[0]
 
   async function renderFormAndSubmitComment(comment: string) {
-    renderRoute(`/articles/${slug}`)
+    renderProtectedRoute(`/articles/${slug}`)
     const textarea = await screen.findByPlaceholderText("Write a comment...")
     // await userEvent.type(textarea, comment)
     userEvent.paste(textarea, comment)
@@ -124,7 +119,7 @@ describe("deleting comment", () => {
   )!
 
   it("shows delete button for own comments only", async () => {
-    renderRoute(`/articles/${slug}`)
+    renderProtectedRoute(`/articles/${slug}`)
     await waitForElementToBeRemoved(screen.getByText(/loading/i))
 
     // prettier-ignore
@@ -134,7 +129,7 @@ describe("deleting comment", () => {
   })
 
   it("deletes comment successfully", async () => {
-    renderRoute(`/articles/${slug}`)
+    renderProtectedRoute(`/articles/${slug}`)
     userEvent.click(await screen.findByTestId(`btn-delete-${comment.id}`))
     await waitForElementToBeRemoved(screen.getByText(comment.body))
   })
---
+++ src/__tests__/authenticated/NewArticlePage.tsx
@@ -0,0 +1,147 @@
+import { screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+
+import config from "../../common/config"
+import * as dummy from "../../../testUtils/dummy"
+import {
+  server as mockServer,
+  rest as mswRest,
+} from "../../../testUtils/mocks/server"
+import { renderProtectedRoute } from "../../../testUtils/render"
+
+const { baseUrl } = config.api
+
+const addTag = async (tag: string) => {
+  userEvent.click(screen.getByText(/New Tag/))
+  await userEvent.type(screen.getByPlaceholderText("Enter tag..."), tag)
+  userEvent.click(screen.getByRole("button", { name: /Add/ }))
+}
+
+const submitForm = async (article: {
+  title: string
+  description: string
+  body: string
+  tags: string[]
+}) => {
+  const { title, description, body, tags } = article
+  await userEvent.type(screen.getByPlaceholderText("Article Title"), title)
+  // prettier-ignore
+  await userEvent.type(screen.getByPlaceholderText("What's this article about?"), description)
+  // prettier-ignore
+  await userEvent.type(screen.getByPlaceholderText("Write your article (in markdown)"), body)
+  for (let i = 0; i < tags.length; i++) {
+    await addTag(tags[i])
+  }
+  userEvent.click(screen.getByRole("button", { name: "Publish Article" }))
+}
+
+describe("validates input", () => {
+  const baseData = dummy.articleData()
+
+  async function executeTest(
+    testData: Partial<typeof baseData>,
+    errMsgPattern: RegExp
+  ) {
+    renderProtectedRoute("/post")
+    await submitForm(Object.assign({}, baseData, testData))
+    expect(await screen.findByText(errMsgPattern)).toBeInTheDocument()
+  }
+
+  it("rejects blank title", async () => {
+    await executeTest({ title: "" }, /Title is required/)
+  })
+
+  it("rejects title shorter than 3 characters", async () => {
+    await executeTest({ title: "ab" }, /Title must have at least 3 characters/)
+  })
+
+  it("rejects title longer than 255 characters", async () => {
+    await executeTest(
+      { title: "a long title goes here".repeat(15) },
+      /Title must not exceed 255 characters/
+    )
+  })
+
+  it("rejects blank description", async () => {
+    await executeTest({ description: "" }, /Description is required/)
+  })
+
+  it("rejects description shorter than 3 characters", async () => {
+    await executeTest(
+      { description: "be" },
+      /Description must have at least 3 characters/
+    )
+  })
+
+  it("rejects description longer than 1024 characters", async () => {
+    await executeTest(
+      { description: "a long description goes here... ".repeat(33) },
+      /Description must not exceed 1024 characters/
+    )
+  })
+
+  it("rejects blank body", async () => {
+    await executeTest({ body: "" }, /Body is required/)
+  })
+
+  it("rejects body shorter than 3 characters", async () => {
+    await executeTest({ body: "be" }, /Body must have at least 3 characters/)
+  })
+
+  // this test passes but is a bit slow
+  xit("rejects body longer than 10000 characters", async () => {
+    await executeTest(
+      { body: "a very long article body goes here...".repeat(280) },
+      /Body must not exceed 10000 characters/
+    )
+  })
+})
+
+describe("tag list", () => {
+  it("adds new tag", async () => {
+    const tag = "blah"
+    renderProtectedRoute("/post")
+    await addTag(tag)
+    expect(screen.getByText(tag)).toBeInTheDocument()
+  })
+
+  it("ignore same tags", async () => {
+    const tag = "blah"
+    renderProtectedRoute("/post")
+    await addTag(tag)
+    await addTag(tag)
+    expect(screen.getAllByText(tag).length).toBe(1)
+  })
+
+  it("removes tag from list", async () => {
+    const tag = "blah"
+    renderProtectedRoute("/post")
+    await addTag(tag)
+    userEvent.click(screen.getByTestId(`btn-delete-${tag}`))
+    expect(screen.queryByText(tag)).not.toBeInTheDocument()
+  })
+})
+
+it("shows unexpected server error", async () => {
+  mockServer.use(
+    mswRest.post(`${baseUrl}/articles`, (req, res, ctx) =>
+      res(ctx.status(500, "Some unknown error"))
+    )
+  )
+
+  jest.spyOn(console, "error").mockImplementation(() => {})
+  renderProtectedRoute("/post")
+  await submitForm(dummy.articleData())
+  // prettier-ignore
+  expect(await screen.findByText(/Unexpected error\. Please try again later\./)).toBeInTheDocument()
+})
+
+it("goes to article page after successful creation", async () => {
+  const { title, description, body, tags } = dummy.articleData()
+  renderProtectedRoute("/post")
+  await submitForm({ title, description, body, tags })
+  // prettier-ignore
+  expect((await screen.findAllByRole("link", { name: /Edit Article/ }))[0]).toBeInTheDocument()
+  expect(screen.getByText(title)).toBeInTheDocument()
+  tags.forEach((tag) => expect(screen.getByText(tag)).toBeInTheDocument())
+})
